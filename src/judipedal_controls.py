fa1d88125c6fcff75e69530e50d22f2f8bf0215b
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-02-21 19:26:19 -0500

    Implemented JudiPedals controls

--- src/judipedal_controls.py
+++ src/judipedal_controls.py
@@ -1,56 +1,101 @@
 from qt import *
 from devices import DeviceManager
+import json
+
+
+class PedalActions(QWidget):
+    def __init__(self, name, obs, parent=None):
+        super().__init__(parent=parent)
+        self.obs = obs
+
+        self.name = QLabel(name)
+        self.status = QLabel('up')
+        self.pressed_action = PersistentLineEdit(f'{name}_pressed')
+        self.released_action = PersistentLineEdit(f'{name}_released')
+
+        with CVBoxLayout(self) as layout:
+            with layout.hbox(align='left'):
+                layout.add(QLabel('Pedal:'))
+                layout.add(self.name)
+                layout.add(QLabel(), 1)
+                layout.add(QLabel('Status:'))
+                layout.add(self.status)
+                
+            with layout.hbox():
+                with layout.vbox():
+                    layout.add(QLabel('Pressed:'))
+                    layout.add(QLabel('Released:'))
+                with layout.vbox():
+                    layout.add(self.pressed_action)
+                    layout.add(self.released_action)
+
+    def pressed(self):
+        self.status.setText('down')
+        if self.pressed_action.text():
+            try:
+                payload = json.loads(self.pressed_action.text())
+                self.obs.send(payload)
+            except:
+                pass
+
+    def released(self):
+        self.status.setText('up')
+        if self.released_action.text():
+            try:
+                payload = json.loads(self.released_action.text())
+                self.obs.send(payload)
+            except:
+                pass
 
 
 @DeviceManager.subscribe_to("judipedals")
 class JudiPedalsControls(QWidget):
-    pedal_pressed = Signal(int)
-
     def __init__(self, parent=None, obs=None):
         super().__init__(parent=parent)
-        self.one = QPushButton(checkable=True)
-        self.two = QPushButton(checkable=True)
-        self.three = QPushButton(checkable=True)
-        self.four = QPushButton(checkable=True)
+
+        self.obs = obs
+
+        self.one = PedalActions('one', obs)
+        self.two = PedalActions('two', obs)
+        self.three = PedalActions('three', obs)
+        self.four = PedalActions('four', obs)
+
+        self.status = QLabel("No Pedals Connected")
         
-        with CHBoxLayout(self) as layout:
-            with layout.vbox(align='top') as layout:
-                layout.add(QLabel('1'))
-                layout.add(self.one)
-            with layout.vbox(align='top') as layout:
-                layout.add(QLabel('2'))
-                layout.add(self.two)
-            with layout.vbox(align='top') as layout:
-                layout.add(QLabel('3'))
-                layout.add(self.three)
-            with layout.vbox(align='top') as layout:
-                layout.add(QLabel('4'))
-                layout.add(self.four)
+        with CVBoxLayout(self, align='top') as layout:
+            with layout.hbox(align='left'):
+                layout.add(QLabel('Status:'))
+                layout.add(self.status)
+            layout.add(HLine())
+            layout.add(self.one)
+            layout.add(HLine())
+            layout.add(self.two)
+            layout.add(HLine())
+            layout.add(self.three)
+            layout.add(HLine())
+            layout.add(self.four)
 
     def connected(self, device):
+        self.status.setText('Pedals Connected')
         device.signals.button_pressed.connect(self.button_pressed)
         device.signals.button_released.connect(self.button_released)
 
     def button_pressed(self, button):
         if button == '1':
-            self.one.setChecked(True)
-            self.pedal_pressed.emit(1)
+            self.one.pressed()
         if button == '2':
-            self.two.setChecked(True)
-            self.pedal_pressed.emit(2)
+            self.two.pressed()
         if button == '3':
-            self.three.setChecked(True)
-            self.pedal_pressed.emit(3)
+            self.three.pressed()
         if button == '4':
-            self.four.setChecked(True)
-            self.pedal_pressed.emit(4)
+            self.four.pressed()
 
     def button_released(self, button):
         if button == '1':
-            self.one.setChecked(False)
+            self.one.released()
         if button == '2':
-            self.two.setChecked(False)
+            self.two.released()
         if button == '3':
-            self.three.setChecked(False)
+            self.three.released()
         if button == '4':
-            self.four.setChecked(False)
+            self.four.released()
