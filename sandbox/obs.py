2e9f12b539a97f7a6f6b6cbc8a0573a74011d4b8
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-03-01 17:40:13 -0500

    Improvements to standard library

--- sandbox/obs.py
+++ sandbox/obs.py
@@ -1,10 +1,13 @@
 # obs
 
+def GetSceneList(cb=empty_cb):
+	def collect_names(message):
+		cb([s['name'] for s in message['scenes']])
+
+	send({"request-type": 'GetSceneList'}, collect_names)
+
 def SetCurrentScene(scene):	
 	send({"request-type":"SetCurrentScene","scene-name":scene})
 
-def GetSceneList():
-    send({"request-type": 'GetSceneList'})
-
 def GetCurrentScene(cb=empty_cb):
-    send({"request-type": 'GetCurrentScene'}, lambda m: cb(m['name']))
+	send({"request-type": 'GetCurrentScene'}, lambda m: cb(m['name']))
--- sandbox/user.py
+++ sandbox/user.py
@@ -1,10 +1,7 @@
 # user
 
-'Whiteboard - Thinkpad'
-
-
 def SaveCurrentScene(location='prev_scene'):
 	GetCurrentScene(cb=lambda x: save(location, x))
 
 def RestorePreviousScene(location='prev_scene'):
-	SetCurrentScene(load('location'))
+	SetCurrentScene(load(location))
