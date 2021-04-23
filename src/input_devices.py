ba5de63cf028002630bac76361bcd2cf6d9a4879
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-04-23 15:59:32 -0400

    Added InputDeviceInfoTab to InputDevices tab

--- src/input_devices.py
+++ src/input_devices.py
@@ -108,6 +108,22 @@ device_widgets = {
 }
 
 
+class InputDeviceInfoTab(QWidget):
+    def __init__(self, known_devices, parent=None):
+        super().__init__(parent=parent)
+    
+        self.known_devices = QListWidget(self)
+        for _, d in known_devices.items():
+            self.known_devices.addItem(f"{d['profile_name']} - {d['guid']}")
+
+        with CHBoxLayout(self) as layout:
+            with layout.vbox(1):
+                layout.add(QLabel("Known Devices:"))
+                layout.add(self.known_devices)
+            with layout.vbox(1):
+                layout.add(QLabel('wat'))
+
+
 @DeviceManager.subscribe
 class InputDeviceManager(QWidget):
     def __init__(self, parent=None):
@@ -115,7 +131,10 @@ class InputDeviceManager(QWidget):
 
         self.known_devices = QSettings().value(f'input_devices/known_devices', {})
         self.widgets = {}
-        self.tabs = QTabWidget(self)
+        self.info_tab = InputDeviceInfoTab(self.known_devices)
+
+        self.tabs = PersistentTabWidget('input_devices/tabs')
+        self.tabs.addTab(self.info_tab, 'Device Info')
 
         with CVBoxLayout(self) as layout:
             layout.add(self.tabs)
@@ -123,9 +142,11 @@ class InputDeviceManager(QWidget):
         for guid, device in self.known_devices.items():
             self.create_widget(guid, device['profile_name'])
 
+        self.tabs.restore_state()
+
     def create_widget(self, guid, profile_name):
         if profile_name in device_widgets:
-            widget = device_widgets[profile_name](self)
+            widget = device_widgets[profile_name](guid, self)
             self.widgets[guid] = widget
             self.tabs.addTab(widget, profile_name)
 
