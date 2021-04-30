185463e6ead18d4e46e0e4bb722aadb6b8755292
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-04-30 16:50:37 -0400

    Added more obs requests to ObsExtension

--- app/plugins/obs_core/obs_extension.py
+++ app/plugins/obs_core/obs_extension.py
@@ -2,9 +2,26 @@ from qtstrap import *
 from stagehand.sandbox import Sandbox
 
 
+# helper function
+def send(payload, cb=None):
+    Sandbox().obs.send(payload, cb)
+
+
 class ObsExtension:
-    def set_scene(self, name):
-        Sandbox().obs.send(requests.SetScene(name))
+    def get_source_list(self, cb=None):
+        send({"request-type": 'GetSourcesList'}, cb)
+
+    def get_scene_list(self, cb=None):
+        send({"request-type": 'GetSceneList'}, cb)
+
+    def get_scene(self, cb=None):
+        send(payload, cb)
+
+    def set_scene(self, scene_name, cb=None):
+        send({"request-type":"SetCurrentScene","scene-name":scene_name}, cb)
+
+    def get_version(self, cb=None):
+        send({"request-type": 'GetVersion'}, cb)
 
     def send(self, payload, cb=None):
-        Sandbox().obs.send(payload, cb)
+        send(payload, cb)
