614c3054fe6a00566eea4b8bbb8dd0ecf6b006e8
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-05-05 22:40:46 -0400

    Removed subtab from InputDeviceManager

--- app/stagehand/input_devices.py
+++ app/stagehand/input_devices.py
@@ -3,49 +3,39 @@ from codex import DeviceManager
 from .actions import ActionWidget, ActionWidgetGroup
 
 
-class InputDeviceInfoTab(QWidget):
-    def __init__(self, known_devices, parent=None):
-        super().__init__(parent=parent)
-    
-        self.known_devices = QListWidget(self)
-        for _, d in known_devices.items():
-            self.known_devices.addItem(f"{d['profile_name']} - {d['guid']}")
-
-        with CHBoxLayout(self) as layout:
-            with layout.vbox(1):
-                layout.add(QLabel("Known Devices:"))
-                layout.add(self.known_devices)
-            with layout.vbox(1):
-                layout.add(QLabel('wat'))
-
-
 @DeviceManager.subscribe
 class InputDeviceManager(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
+        self.parent = parent
 
         self.known_devices = QSettings().value(f'input_devices/known_devices', {})
         self.widgets = {}
-        self.info_tab = InputDeviceInfoTab(self.known_devices)
 
-        self.tabs = PersistentTabWidget('input_devices/tabs')
-        self.tabs.addTab(self.info_tab, 'Device Info')
+        self.known_devices_list = QListWidget(self)
+        for _, d in self.known_devices.items():
+            self.known_devices_list.addItem(f"{d['profile_name']} - {d['guid']}")
 
-        with CVBoxLayout(self) as layout:
-            layout.add(self.tabs)
+        with CHBoxLayout(self) as layout:
+            with layout.vbox(1):
+                layout.add(QLabel("Known Devices:"))
+                layout.add(self.known_devices_list)
+            with layout.vbox(1):
+                layout.add(QLabel('wat'))
 
         for guid, device in self.known_devices.items():
             self.create_widget(guid, device['profile_name'])
-
-        self.tabs.restore_state()
 
     def create_widget(self, guid, profile_name):
         if profile_name in DeviceManager.profiles():
             profile = DeviceManager.profiles()[profile_name]
             if hasattr(profile, 'widget'):
-                widget = profile.widget(guid, self)
+                widget = profile.widget(guid, self.parent)
                 self.widgets[guid] = widget
-                self.tabs.addTab(widget, profile_name)
+                if hasattr(self.parent, 'tabs'):
+                    self.parent.tabs.addTab(widget, profile_name)
+                else:
+                    self.parent.plugin_widgets[profile_name] = widget
 
     def device_added(self, device):
         if device.guid not in self.known_devices:
