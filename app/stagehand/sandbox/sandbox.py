3c0b83c7916990750e99528c78c808df3a988c18
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-05-12 17:53:51 -0400

    Added sandbox eval method

--- app/stagehand/sandbox/sandbox.py
+++ app/stagehand/sandbox/sandbox.py
@@ -82,6 +82,27 @@ class _Sandbox(QWidget):
             error_cb(error)
         else:
             self.tools.print(error)
+    
+    @Slot()
+    def eval(self, text, this=None, error_cb=None):
+        if text == '':
+            return
+
+        error = ''
+        try:
+            code = compile(text, '', 'exec')
+            self._globals['this'] = this
+            eval(code, self._globals, self._locals)
+        except Exception as e:
+            error = str(e)
+
+        self._globals['this'] = None
+
+        if error_cb:
+            error_cb(error)
+        else:
+            self.tools.print(error)
+
 
 
 sandbox = None
