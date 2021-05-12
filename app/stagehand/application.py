117ab09181e6bd282712a6225523d332a53d0b11
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-05-12 18:33:02 -0400

    Removed old get_application() function

--- app/stagehand/application.py
+++ app/stagehand/application.py
@@ -17,22 +17,3 @@ class Application(BaseApplication):
 
         self.device_manager = DeviceManager(self)
         self.aboutToQuit.connect(self.device_manager.close)
-
-
-# def get_application():
-#     app = QApplication.instance()
-
-#     install_ctrlc_handler(app)
-#     install_app_info(app)
-    
-
-#     # icon = QIcon(qta.icon('fa.circle','fa5s.video', options=[{'color':'gray'}, {'scale_factor':0.5, 'color':'white'}]))
-#     # app.setWindowIcon(icon)
-
-#     app.updater = ApplicationUpdater()
-#     # app.updater.check_latest()
-
-#     app.device_manager = DeviceManager(app)
-#     app.aboutToQuit.connect(lambda: app.device_manager.close())
-
-#     return app
