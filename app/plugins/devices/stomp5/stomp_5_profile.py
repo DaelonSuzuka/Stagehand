ec11b8d5622571df36c6ad9ba470c926bf21c6ff
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-05-13 11:02:27 -0400

    Improvements to stomp trigger

--- app/plugins/devices/stomp5/stomp_5_profile.py
+++ app/plugins/devices/stomp5/stomp_5_profile.py
@@ -3,26 +3,33 @@ from codex import SerialDevice, JudiStandardMixin
 from .stomp_5_widget import Stomp5Widget
 
 
-class Signals(QObject):
-    button_pressed = Signal(str)
-    button_released = Signal(str)
+class Adapter(QObject):
+    def __init__(self, signals=None):
+        super().__init__()
+        if signals is None:
+            return
 
-    @property
-    def message_tree(self):
-        return {
-            "update": {
-                "button_pressed": self.button_pressed.emit,
-                "button_released": self.button_released.emit
-            }
-        }
+        for name in dir(signals):
+            if name not in dir(QObject):
+                signal = getattr(signals, name)
+                if isinstance(signal, SignalInstance):
+                    signal.connect(getattr(self, name).emit)
 
 
 class Stomp5(JudiStandardMixin, SerialDevice):
     profile_name = "Stomp 5"
     widget = Stomp5Widget
 
+    class Signals(Adapter):
+        button_pressed = Signal(str)
+        button_released = Signal(str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.signals = Signals()
-        self.message_tree.merge(self.signals.message_tree)
+        self.signals = self.Signals()
         self.message_tree.merge(self.common_message_tree)
+        self.message_tree['update']['button_pressed'] = self.signals.button_pressed.emit
+        self.message_tree['update']['button_released'] = self.signals.button_released.emit
+
+    def get_adapter(self):
+        return self.Signals(self.signals)
--- app/plugins/devices/stomp5/stomp_5_trigger.py
+++ app/plugins/devices/stomp5/stomp_5_trigger.py
@@ -1,7 +1,9 @@
 from qtstrap import *
 from stagehand.actions import TriggerStackItem
+from codex import SubscriptionManager
 
 
+@SubscriptionManager.subscribe
 class Stomp5Trigger(QWidget, TriggerStackItem):
     triggered = Signal()
 
@@ -9,28 +11,67 @@ class Stomp5Trigger(QWidget, TriggerStackItem):
         super().__init__()
         
         self.triggered.connect(run)
+        self.devices = {}
 
-        self.number = QComboBox()
-        self.number.addItems(['1', '2', '3', '4', '5'])
-        self.number.currentIndexChanged.connect(changed)
+        self.device = QComboBox()
+        self.device.currentIndexChanged.connect(changed)
+        self.device.currentTextChanged.connect(self.device_changed)
+
+        self.signal = QComboBox()
+        self.signal.addItems([
+            '1 Pressed',
+            '1 Released',
+            '2 Pressed',
+            '2 Released',
+            '3 Pressed',
+            '3 Released',
+            '4 Pressed',
+            '4 Released',
+            '5 Pressed',
+            '5 Released',
+        ])
+
+        self.pedal = QComboBox()
+        self.pedal.addItems(['1', '2', '3', '4', '5'])
+        self.pedal.currentIndexChanged.connect(changed)
 
         self.direction = QComboBox()
         self.direction.addItems(['press', 'release'])
         self.direction.currentIndexChanged.connect(changed)
 
         with CHBoxLayout(self, margins=(0,0,0,0)) as layout:
-            layout.add(self.number)
-            layout.add(self.direction)
+            layout.add(self.device)
+            layout.add(self.signal)
+    
+    def device_changed(self, guid):
+        if guid in self.devices:
+            adapter = self.devices[guid].get_adapter()
+
+    def device_added(self, device):
+        if device.profile_name == 'Stomp 5':
+            self.devices[device.guid] = device
+            with SignalBlocker(self.device):
+                selected = self.device.currentText()
+                self.device.clear()
+                items = sorted(self.devices.keys())
+                if selected not in items:
+                    items.insert(0, selected)
+                self.device.addItems(items)
+                self.device.setCurrentText(selected)
 
     def from_dict(self, data):
         try:
             self.direction.setCurrentText(data['direction'])
-            self.number.setCurrentText(data['number'])
+            self.pedal.setCurrentText(data['pedal'])
+            if data['device'] not in self.devices:
+                self.device.addItem(data['device'])
+            self.device.setCurrentText(data['device'])
         except:
             pass
 
     def to_dict(self):
         return {
             'direction': self.direction.currentText(),
-            'number': self.number.currentText(),
+            'pedal': self.pedal.currentText(),
+            'device': self.device.currentText(),
         }
