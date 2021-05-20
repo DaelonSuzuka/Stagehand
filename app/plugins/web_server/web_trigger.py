403274d367ca94d89887298318caad0540a93b2c
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-05-20 02:46:14 -0400

    Shortened code

--- app/plugins/web_server/web_trigger.py
+++ app/plugins/web_server/web_trigger.py
@@ -17,8 +17,7 @@ class WebTrigger(QWidget, TriggerItem):
         self.socket.message_recieved.connect(self.processTextMessage)
 
         self.action = QComboBox()
-        items = [f'Action {i + 1}' for i in range(12)]
-        self.action.addItems(items)
+        self.action.addItems([f'Action {i + 1}' for i in range(12)])
         self.action.currentIndexChanged.connect(changed)
 
         with CHBoxLayout(self, margins=(0,0,0,0)) as layout:
