726a73209faa90915084d9d968e7fbe6506d6ce4
David Kincaid <daelonsuzuka@gmail.com>
2022-11-26 22:43:03 -0500

    Prep work for future plugin package system

--- app/plugins/autohotkey/ahk_extension.py
+++ app/plugins/autohotkey/ahk_extension.py
@@ -1,13 +1,21 @@
 from stagehand.sandbox import SandboxExtension
 from ahk.daemon import AHK, AHKDaemon
 
+# for future vendored packages
+# from .packages.ahk.daemon import AHK, AHKDaemon
+# from pathlib import Path
+
 
 class AutohotkeyExtension(SandboxExtension):
     name = ['autohotkey', 'ahk']
 
     def __init__(self):
-        self._ahk = AHK()
+        # for future vendored packages
+        # exe_path = (Path(__file__).parent / 'packages/Scripts/AutoHotkey.exe').as_posix()
+        # self._ahk = AHK(executable_path=exe_path)
+        # self._daemon = AHKDaemon(executable_path=exe_path)
 
+        self._ahk = AHK()
         self._daemon = AHKDaemon()
         self._daemon.start()
 
@@ -16,3 +24,5 @@ class AutohotkeyExtension(SandboxExtension):
             return getattr(self._ahk, name)
 
         return getattr(self._daemon, name)
+
+    def close
