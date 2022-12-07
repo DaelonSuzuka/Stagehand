2ca17d4c8741a115604d39b09132716560763a68
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-12-07 18:54:51 -0500

    Autohotkey plugin only works on windows

--- app/plugins/autohotkey/__init__.py
+++ app/plugins/autohotkey/__init__.py
@@ -1,2 +1,7 @@
-from .ahk_action import AutohotkeyAction
-from .ahk_extension import AutohotkeyExtension
+import sys
+
+
+# this plugin only works on windows, sorry
+if sys.platform == 'win32':
+    from .ahk_action import AutohotkeyAction
+    from .ahk_extension import AutohotkeyExtension
