e7052b81b70a7037fd6b6ab35cee812d7dcd1000
David Kincaid <daelonsuzuka@gmail.com>
2022-12-08 13:13:51 -0500

    Fix ExecutableNotFoundError

--- app/plugins/autohotkey/ahk_extension.py
+++ app/plugins/autohotkey/ahk_extension.py
@@ -1,10 +1,7 @@
 from stagehand.sandbox import SandboxExtension
-from ahk.daemon import AHK, AHKDaemon
-from ahk.directives import NoTrayIcon
-
-# for future vendored packages
-# from .packages.ahk.daemon import AHK, AHKDaemon
-# from pathlib import Path
+from ahk.daemon import AHK
+from pathlib import Path
+import sys
 
 
 class AutohotkeyExtension(SandboxExtension):
@@ -12,19 +9,11 @@ class AutohotkeyExtension(SandboxExtension):
 
     def __init__(self):
         # for future vendored packages
-        # exe_path = (Path(__file__).parent / 'packages/Scripts/AutoHotkey.exe').as_posix()
+        # exe_path = (Path(ahk.__file__).parent / 'packages/Scripts/AutoHotkey.exe').as_posix()
         # self._ahk = AHK(executable_path=exe_path)
-        # self._daemon = AHKDaemon(executable_path=exe_path)
 
-        self._ahk = AHK(directives=[NoTrayIcon])
-        self._daemon = AHKDaemon(directives=[NoTrayIcon])
-        qApp.aboutToQuit.connect(self._daemon.stop)
-        self._daemon.start()
+        exe_path = (Path(sys.executable).parent / 'AutoHotkey.exe').as_posix()
+        self._ahk = AHK(executable_path=exe_path)
 
     def __getattr__(self, name):
-        if name in ['run_script']:
-            return getattr(self._ahk, name)
-
-        return getattr(self._daemon, name)
-
-    # def close
+        return getattr(self._ahk, name)
