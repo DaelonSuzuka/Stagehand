7c557578af4e43af6a0e7a0631f4c58ee693edac
David Kincaid <daelonsuzuka@gmail.com>
2022-12-08 23:53:31 -0500

    Add log flushing so hopefully plugin loader will actually log errors

--- app/stagehand/plugin_loader.py
+++ app/stagehand/plugin_loader.py
@@ -13,6 +13,10 @@ import logging
 plugin_folder = OPTIONS.APPLICATION_PATH / 'plugins'
 
 
+def flush_logs():
+    logging.getLogger().handlers[0].flush()
+
+
 @singleton
 class Plugins:
     _plugins = {}
@@ -43,6 +47,7 @@ class Plugins:
 
         if plugin_name not in self._plugins:
             self.log.info(f'Attemping to load zip plugin: {plugin}')
+            flush_logs()
             sys.path.insert(0, plugin)
 
             try:
@@ -50,8 +55,10 @@ class Plugins:
                 module = importer.load_module(plugin_name)
                 self._plugins[plugin_name] = module
                 self.log.debug(f'Successfully loaded zip plugin: {plugin_name}')
+                flush_logs()
             except Exception as e:
                 self.log.error(f'Failed loading plugin: {plugin_name}', exc_info=True)
+                flush_logs()
 
     def load_loose_plugin(self, plugin):
         plugin_name = plugin.relative_to(OPTIONS.APPLICATION_PATH).as_posix()
@@ -59,14 +66,18 @@ class Plugins:
 
         if plugin_name not in self._plugins:
             self.log.info(f'Attemping to load loose plugin: {plugin}')
+            flush_logs()
             try:
                 package_dir = plugin / 'packages'
                 if package_dir.exists():
                     self.log.info(f'Adding plugin package dir to path: {plugin}')
+                    flush_logs()
                     sys.path.insert(0, package_dir.as_posix())
 
                 module = importlib.import_module(plugin_name)
                 self._plugins[plugin_name] = module
                 self.log.debug(f'Successfully loaded loose plugin: {plugin_name}')
+                flush_logs()
             except Exception as e:
                 self.log.error(f'Failed loading plugin: {plugin_name}', exc_info=True)
+                flush_logs()
