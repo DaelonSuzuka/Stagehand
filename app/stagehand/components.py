f562aa6a9acf242d89f35e22d3ee257612aad20a
David Kincaid <daelonsuzuka@gmail.com>
2023-03-03 03:05:56 -0500

    Removed old components

--- app/stagehand/components.py
+++ app/stagehand/components.py
@@ -3,19 +3,6 @@ import qtawesome as qta
 from abc import abstractmethod
 
 
-class SidebarButton(QPushButton):
-    def __init__(self, *args, target=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setIconSize(QSize(40, 40))
-        self.setFlat(True)
-        self.target = target
-        self.clicked.connect(self.on_click)
-
-    def on_click(self):
-        self.parent().parent().set_widget(self.target)
-        self.setEnabled(False)
-
-
 class StagehandPage(QWidget):
     page_type = ''
     tags = ['user']
@@ -39,14 +26,6 @@ class StagehandPage(QWidget):
             'page_type': self.page_type,
         }
         return data
-
-
-class StagehandWidget(QWidget):
-    def __init__(self, *args, icon_name='', **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if icon_name:
-            self.sidebar_button = SidebarButton(parent=self, target=self, icon=qta.icon(icon_name))
 
 
 class StagehandStatusBarItem(QWidget):
