8cdaebc8177c2aa6811b7c4cfcaee8a7b5547fa6
David Kincaid <daelonsuzuka@gmail.com>
2023-03-04 12:03:24 -0500

    Update cyberlang package and implement new module syntax

--- app/plugins/cyber/cyber_extension.py
+++ app/plugins/cyber/cyber_extension.py
@@ -10,11 +10,9 @@ class CyberExtension(SandboxExtension):
         self.vm = CyberVM()
 
         with self.vm.module('core') as module:
-            @module('print', 1)
-            def _print(vm, args, nargs):
-                s = cyValueToTempString(vm, args[0])
-                Sandbox().tools.print(s.charz.decode())
-                return 0
+            @module.function('print')
+            def _print(string: str):
+                Sandbox().tools.print(string)
 
     def eval(self, string):
         self.vm.eval(string)
