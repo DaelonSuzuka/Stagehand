3ea58deca6d550b195a917d994d5bf7dc28d37fa
David Kincaid <daelonsuzuka@gmail.com>
2023-04-04 14:57:29 -0400

    Update Cyber custom module definitions

--- app/plugins/cyber/cyber_extension.py
+++ app/plugins/cyber/cyber_extension.py
@@ -9,10 +9,16 @@ class CyberExtension(SandboxExtension):
     def __init__(self):
         self.vm = CyberVM()
 
-        with self.vm.module('core') as module:
-            @module.function('print')
-            def _print(string: str):
+        @self.vm.module('core')
+        class Core:
+            def print(self, string: str):
                 Sandbox().tools.print(string)
+
+        @self.vm.module
+        class Stagehand:
+            """Cyber module for Stagehand API"""
+            pass
+
 
     def eval(self, string):
         self.vm.eval(string)
