7ce983f8a711b096c42a0352d8fb3dd5e8fda081
Daelon Suzuka <daelonsuzuka@gmail.com>
2024-02-02 04:00:35 -0500

    Disable saleae plugin

--- app/plugins/saleae/__init__.py
+++ app/plugins/saleae/__init__.py
@@ -1,3 +1,3 @@
-from .saleae_extension import SaleaeExtension
-from .saleae_settings_page import SaleaeSettingsPage
-from .saleae_action import SaleaeAction
+# from .saleae_extension import SaleaeExtension
+# from .saleae_settings_page import SaleaeSettingsPage
+# from .saleae_action import SaleaeAction
