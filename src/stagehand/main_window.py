f496488a5e3ddf08bee87fd3b2d9b7dcbd7965cd
David Kincaid <daelonsuzuka@gmail.com>
2024-12-19 20:40:16 -0500

    Trying to fix errors

--- src/stagehand/main_window.py
+++ src/stagehand/main_window.py
@@ -150,8 +150,8 @@ class MainWindow(BaseMainWindow):
 
     def init_tray_stuff(self):
         self.tray_icon = QSystemTrayIcon(self)
-        if files := list(Path(OPTIONS.APPLICATION_PATH).rglob(OPTIONS.app_info.AppIconName)):
-            self.tray_icon.setIcon(QIcon(files[0].as_posix()))
+        # if files := list(Path(OPTIONS.APPLICATION_PATH).rglob(OPTIONS.app_info.AppIconName)):
+        #     self.tray_icon.setIcon(QIcon(files[0].as_posix()))
 
         self.tray_menu = QMenu()
         self.tray_menu.addAction(QAction('Open', self, triggered=self.show))
